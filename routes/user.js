bf1b5abd9ce47393d3e4d64b026af0a6f33fb1a9
vignesh_andalam <vigneshandalam4@gmail.com>
2025-02-08 23:26:49 +0530

    Updated policies.ejs with new sections

--- routes/user.js
+++ routes/user.js
@@ -51,4 +51,9 @@ router.get('/profile', isLoggedIn, (req, res) => {
     res.render('miscellaneous/profile.ejs', { user });
 });
 
+//policies
+router.get("/policies",(req,res) => {
+    res.render("miscellaneous/policies.ejs");
+});
+
 module.exports = router;
